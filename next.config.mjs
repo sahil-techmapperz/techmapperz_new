017a12b8d0b66a065a8ce2c120f8c849c96f975e
Sahil90 <sahil.laskar@techmapperz.com>
2026-02-23 16:07:42 +0530

    fix: proxy static files to nextjs and allow custom image qualities

--- next.config.mjs
+++ next.config.mjs
@@ -91,6 +91,7 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    qualities: [25, 50, 75, 80, 85, 90, 100],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
